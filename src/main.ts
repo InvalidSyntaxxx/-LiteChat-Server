3b481008de2230b56267fef324f03721a06330e6
Redamancy9189 <1791081494@qq.com>
2023-02-28 13:42:26 +0800

    feat：add SSL support

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@
  * @Author: 王远昭
  * @Date: 2023-02-25 12:17:23
  * @LastEditors: 王远昭
- * @LastEditTime: 2023-02-26 23:22:57
+ * @LastEditTime: 2023-02-28 13:41:56
  */
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -14,9 +14,22 @@ import { logger } from './common/middleware/logger.middleware';
 import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 import { join } from 'path';
 
+import * as fs from 'fs';
+
 async function bootstrap() {
+  // 小伙伴们，这里读取的https的证书位置，一定一定要是绝对路劲噢！！！
+  const httpsOptions = {
+    key: fs.readFileSync(
+      join(__dirname, '../secrets/ssl.key'),
+    ),
+    cert: fs.readFileSync(
+      join(__dirname, '../secrets/ssl.pem'),
+    ),
+  };
   // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{
+    httpsOptions  //开启https访问
+  });
 
   // 允许跨域
   app.enableCors({ 
